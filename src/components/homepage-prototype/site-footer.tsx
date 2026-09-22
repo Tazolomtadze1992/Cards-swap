94a8c05c470f8f4dda43d596bfc04809b5a4eaaf
Tazo Lomtadze <lomtadzet92@gmail.com>
2026-09-22 16:50:10 +0400

    Refine footer branding and typography

--- src/components/homepage-prototype/site-footer.tsx
+++ src/components/homepage-prototype/site-footer.tsx
@@ -1,9 +1,17 @@
 import styles from "./site-footer.module.css";
 
 export function SiteFooter({ appearance = "default" }: { appearance?: "default" | "embedded" }) {
+  const logoSrc = appearance === "embedded"
+    ? "/assets/homepage/council-of-europe-logo.png"
+    : "/assets/homepage/council-of-europe-logo-on-cream.png";
+
   return <footer className={styles.footer} data-appearance={appearance}>
     <div className={styles.content}>
       <p>ეს ვებგვერდი შექმნილია ევროპის საბჭოსა და საგანმანათლებლო დაწესებულების მანდატურის სამსახურის თანამშრომლობის შედეგად. ის წარმოადგენს არაინსტიტუციურ ვებგვერდს. არსებულ პლატფორმაზე გამოთქმული მოსაზრებები წარმოადგენს ავტორების პასუხისმგებლობას და არ ასახავს ევროპის საბჭოს ოფიციალურ პოლიტიკას.</p>
+      <div className={styles.logo}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={logoSrc} width={296} height={238} alt="ევროპის საბჭო" />
+      </div>
     </div>
   </footer>;
 }
