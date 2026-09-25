85efd9754cb877bcb45e7bc06c21e25cdddabfea
Tazo Lomtadze <lomtadzet92@gmail.com>
2026-09-25 15:19:26 +0400

    Refine homepage disclaimer footer

--- src/components/homepage-prototype/site-footer.tsx
+++ src/components/homepage-prototype/site-footer.tsx
@@ -3,8 +3,14 @@ import styles from "./site-footer.module.css";
 export function SiteFooter() {
   return <footer className={styles.footer}>
     <div className={styles.content}>
-      <p>ეს ვებგვერდი შექმნილია ევროპის საბჭოსა და საგანმანათლებლო დაწესებულების მანდატურის სამსახურის თანამშრომლობის შედეგად. არსებულ პლატფორმაზე გამოთქმული მოსაზრებები წარმოადგენს ავტორების პასუხისმგებლობას და არ ასახავს ევროპის საბჭოს ოფიციალურ პოლიტიკას.</p>
-      <p lang="en">This site results from co-operation between the Council of Europe and the Office of Resource Officers of Educational Institutions. The opinions expressed in this work are the responsibility of the authors and do not necessarily reflect the official policy of the Council of Europe.</p>
+      <div className={styles.disclaimer}>
+        <p>ეს ვებგვერდი შექმნილია ევროპის საბჭოსა და საგანმანათლებლო დაწესებულების მანდატურის სამსახურის თანამშრომლობის შედეგად. არსებულ პლატფორმაზე გამოთქმული მოსაზრებები წარმოადგენს ავტორების პასუხისმგებლობას და არ ასახავს ევროპის საბჭოს ოფიციალურ პოლიტიკას.</p>
+        <p lang="en">This site results from co-operation between the Council of Europe and the Office of Resource Officers of Educational Institutions. The opinions expressed in this work are the responsibility of the authors and do not necessarily reflect the official policy of the Council of Europe.</p>
+      </div>
+      <div className={styles.details}>
+        <p>ქ. თბილისი, მ. ალექსიძის II შესახვევი, N2 ტელ: 2200 220 (3025), 08 000 000 88</p>
+        <p>© EMIS 2026 ყველა უფლება დაცულია</p>
+      </div>
     </div>
   </footer>;
 }
