def62bca918654095d504f4242d4021fa2eefbfe
Tazo Lomtadze <lomtadzet92@gmail.com>
2026-09-25 13:37:36 +0400

    Update homepage disclaimer layout

--- src/components/homepage-prototype/site-footer.tsx
+++ src/components/homepage-prototype/site-footer.tsx
@@ -3,8 +3,8 @@ import styles from "./site-footer.module.css";
 export function SiteFooter() {
   return <footer className={styles.footer}>
     <div className={styles.content}>
-      <p className={styles.georgian}>ეს ვებგვერდი შექმნილია ევროპის საბჭოსა და საგანმანათლებლო დაწესებულების მანდატურის სამსახურის თანამშრომლობის შედეგად. ის წარმოადგენს არაინსტიტუციურ ვებგვერდს. არსებულ პლატფორმაზე გამოთქმული მოსაზრებები წარმოადგენს ავტორების პასუხისმგებლობას და არ ასახავს ევროპის საბჭოს ოფიციალურ პოლიტიკას.</p>
-      <p className={styles.english} lang="en">This site results from co-operation between the Council of Europe and the Office of Resource Officers of Educational Institutions. It is a non-institutional site. The opinions expressed in this work are the responsibility of the authors and do not necessarily reflect the official policy of the Council of Europe.</p>
+      <p>ეს ვებგვერდი შექმნილია ევროპის საბჭოსა და საგანმანათლებლო დაწესებულების მანდატურის სამსახურის თანამშრომლობის შედეგად. არსებულ პლატფორმაზე გამოთქმული მოსაზრებები წარმოადგენს ავტორების პასუხისმგებლობას და არ ასახავს ევროპის საბჭოს ოფიციალურ პოლიტიკას.</p>
+      <p lang="en">This site results from co-operation between the Council of Europe and the Office of Resource Officers of Educational Institutions. The opinions expressed in this work are the responsibility of the authors and do not necessarily reflect the official policy of the Council of Europe.</p>
     </div>
   </footer>;
 }
